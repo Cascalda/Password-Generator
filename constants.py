c2eb2334faa92e55dd53de8dcfff21a074b7e7db
Granite7 <111849193+Cascalda@users.noreply.github.com>
2024-04-13 19:36:41 +0800

    Adjusted gaps between CONSTANTS

--- constants.py
+++ constants.py
@@ -8,16 +8,23 @@ MIN_LENGTH: int = 6
 MAX_LENGTH: int = 15
 RANGE_LENGTH: tuple[int, int] = (MIN_LENGTH, MAX_LENGTH)
 
+
 MIN_PASSWORD_CHAR: int = 8
 MAX_PASSWORD_CHAR: int = 128
 RANGE_PASSWORD_CHAR: tuple[int, int] = (MIN_PASSWORD_CHAR, MAX_PASSWORD_CHAR)
+
 
 MIN_PASSPHRASE_WORDS: int = 4
 MAX_PASSPHRASE_WORDS: int = 20
 RANGE_PASSPHRASE_WORDS: tuple[int, int] = (MIN_PASSPHRASE_WORDS, MAX_PASSPHRASE_WORDS)
 
+
 MAX_SEPARATOR_LENGTH: int = 3
 DEFAULT_SEPARATOR: str = "_"
+
+
+RANDOM_CAPS_DISPLAY: list[str] = ["first", "last", "any-one", "all"]
+
 
 CHARACTERS: dict[str, str] = {
     "lowercase": string.ascii_lowercase,
@@ -25,7 +32,5 @@ CHARACTERS: dict[str, str] = {
     "numbers": string.digits,
     "special characters": string.punctuation,
 }
-
-RANDOM_CAPS_DISPLAY: list[str] = ["first", "last", "any-one", "all"]
 
 WORDS: list[str] = sorted(get_english_words_set(sources=["web2"]))
