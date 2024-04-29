c8ef0de6d959ee466d4db7005679507e804447e4
Granite7 <111849193+Cascalda@users.noreply.github.com>
2024-04-29 19:16:29 +0800

    Remove type annotations, it became too complicated to do type annotations for CONSTANTS in python since CONSTANTS are not enforced.

--- constants.py
+++ constants.py
@@ -5,36 +5,36 @@ import string
 from english_words import get_english_words_set
 
 
-QUIT_COMMANDS: list[str] = ["quit", "q", "exit", "e"]
+QUIT_COMMANDS = ("quit", "q", "exit", "e")
 
 
-MIN_LENGTH: int = 6
-MAX_LENGTH: int = 15
-RANGE_LENGTH: tuple[int, int] = (MIN_LENGTH, MAX_LENGTH)
+MIN_LENGTH = 6
+MAX_LENGTH = 15
+RANGE_LENGTH = (MIN_LENGTH, MAX_LENGTH)
 
 
-MIN_PASSWORD_CHAR: int = 8
-MAX_PASSWORD_CHAR: int = 128
-RANGE_PASSWORD_CHAR: tuple[int, int] = (MIN_PASSWORD_CHAR, MAX_PASSWORD_CHAR)
+MIN_PASSWORD_CHAR = 8
+MAX_PASSWORD_CHAR = 128
+RANGE_PASSWORD_CHAR = (MIN_PASSWORD_CHAR, MAX_PASSWORD_CHAR)
 
 
-MIN_PASSPHRASE_WORDS: int = 4
-MAX_PASSPHRASE_WORDS: int = 20
-RANGE_PASSPHRASE_WORDS: tuple[int, int] = (MIN_PASSPHRASE_WORDS, MAX_PASSPHRASE_WORDS)
+MIN_PASSPHRASE_WORDS = 4
+MAX_PASSPHRASE_WORDS = 20
+RANGE_PASSPHRASE_WORDS = (MIN_PASSPHRASE_WORDS, MAX_PASSPHRASE_WORDS)
 
 
-MAX_SEPARATOR_LENGTH: int = 3
-DEFAULT_SEPARATOR: str = "_"
+MAX_SEPARATOR_LENGTH = 3
+DEFAULT_SEPARATOR = "_"
 
 
-RANDOM_CAPS_DISPLAY: list[str] = ["first", "last", "any-one", "all"]
+RANDOM_CAPS_DISPLAY = ("first", "last", "any-one", "all")
 
 
-CHARACTERS: dict[str, str] = {
+CHARACTERS = {
     "lowercase": string.ascii_lowercase,
     "uppercase": string.ascii_uppercase,
     "numbers": string.digits,
     "special characters": string.punctuation,
 }
 
-WORDS: list[str] = sorted(get_english_words_set(sources=["web2"]))
+WORDS = sorted(get_english_words_set(sources=["web2"]))
